42ae4ccdc83eaf9ec9fd446cb35fe8783c0252cb
arronburgess <generalkajgue@hotmail.com>
2025-12-15 19:43:23 +0000

    chore: added some code in the server API for getting table results

--- server/server.js
+++ server/server.js
@@ -19,3 +19,17 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to the server. GET comfy!" });
   //   res.send("<h1>Welcome to the server. GET comfy!</h1>");
 });
+
+//============================================================================
+// Arron's code
+// Getting some data from the DB relating to fetching event table rows
+// ORDER BY id DESC LIMIT 16 defines how many results we want to get from the database, we can change this number if the page isn't populated with enough events.
+app.get("/eventdisplay", async function (req, res) {
+  const hostedEventsGet = await db.query(
+    `SELECT * FROM events ORDER BY id DESC LIMIT 16;`
+  );
+  console.log(hostedEventsGet);
+  res.json(hostedEventsGet.rows);
+});
+
+//============================================================================
