ac31c316e24121f49d4d46a234ece77c301d70b4
Dylan Marley <dylanjmarley01@gmail.com>
2025-12-17 13:06:27 +0000

    feat: events and comments on page

--- client/displayevent/displayevent.js
+++ client/displayevent/displayevent.js
@@ -1,5 +1,73 @@
 const commentForm = document.querySelector("#commentForm");
 
+// get event id from the URL
+const urlID = new URLSearchParams(window.location.search);
+const selectedEventId = urlID.get("id");
+
+function renderEvent(data, container) {
+  container.innerHTML = "";
+
+  const insertedEvent = document.createElement("div");
+  insertedEvent.classList.add("inserted-event");
+
+  const eventName = document.createElement("h3");
+  eventName.classList.add("event-name");
+  eventName.textContent = data.event_name;
+
+  const hostName = document.createElement("p");
+  hostName.classList.add("host-name");
+  hostName.textContent = `Host Name: ${data.host_name}`;
+
+  const eventDescription = document.createElement("p");
+  eventDescription.classList.add("event-description");
+  eventDescription.textContent = `Description: ${data.event_description}`;
+
+  const eventLocation = document.createElement("p");
+  eventLocation.classList.add("event-location");
+  eventLocation.textContent = `Location: ${data.location}`;
+
+  const eventDate = document.createElement("p");
+  eventDate.classList.add("event-date");
+  eventDate.textContent = `Date: ${data.date}`;
+
+  const eventTime = document.createElement("p");
+  eventTime.classList.add("event-time");
+  eventTime.textContent = `Time: ${data.time}`;
+
+  const eventCategory = document.createElement("p");
+  eventCategory.classList.add("event-category");
+  eventCategory.textContent = `Category: ${data.category}`;
+
+  insertedEvent.append(
+    eventName,
+    hostName,
+    eventDescription,
+    eventLocation,
+    eventDate,
+    eventTime,
+    eventCategory
+  );
+  container.appendChild(insertedEvent);
+}
+
+async function fetchEvents() {
+  const response = await fetch(
+    `http://localhost:8080/events/${selectedEventId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+
+  const eventsContainer = document.getElementById("event-details-container");
+  renderEvent(data, eventsContainer);
+}
+fetchEvents();
+
 async function handleSubmitCommentForm(event) {
   event.preventDefault();
 
@@ -12,16 +80,17 @@ async function handleSubmitCommentForm(event) {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ name, comment }),
+    body: JSON.stringify({ selectedEventId, name, comment }),
   });
 
   document.getElementById("commentForm").reset();
-  await fetchComments();
+  await fetchComments(selectedEventId);
 }
 
 commentForm.addEventListener("submit", handleSubmitCommentForm);
 
 async function fetchComments(eventId) {
+  console.log("Fetching comments for event ID:", selectedEventId);
   const response = await fetch(
     `http://localhost:8080/events/comments?event_id=${eventId}`,
     {
@@ -51,4 +120,4 @@ async function fetchComments(eventId) {
   });
 }
 
-fetchComments(2);
+fetchComments(selectedEventId);
